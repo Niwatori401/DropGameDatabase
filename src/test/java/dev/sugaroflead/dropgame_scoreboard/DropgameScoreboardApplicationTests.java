f0a64a7faf1f6119f306a09618540354e6d85057
Niwatori401 <cjackson@firemail.cc>
2023-10-30 10:26:10 +0900

    Add Spring Boot files

---
+++ src/test/java/dev/sugaroflead/dropgame_scoreboard/DropgameScoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sugaroflead.dropgame_scoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropgameScoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
