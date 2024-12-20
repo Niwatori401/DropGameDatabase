c371f06102120cb0a6d8a5e1b3fed847ae9692b2
Niwatori401 <->
2024-12-19 18:02:22 -0800

    Fix compilation error in last commit

--- src/main/java/party/niwatori/dropgame_scoreboard/DropgameScoreboardApplication.java
+++ src/main/java/party/niwatori/dropgame_scoreboard/DropgameScoreboardApplication.java
@@ -9,7 +9,7 @@ import java.util.concurrent.CountDownLatch;
 @EnableJpaAuditing // Enables the use of @CreatedDate and @LastModifiedDate annotations
 public class DropgameScoreboardApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(DropgameScoreboardApplication.class, args);
         new CountDownLatch(1).await();
 	}
