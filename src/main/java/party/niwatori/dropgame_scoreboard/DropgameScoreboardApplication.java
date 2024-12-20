9a3efb4f590b685917dd9ab128a8d54755dd6357
Niwatori401 <->
2024-12-19 17:59:54 -0800

    Prevent app from closing itself

--- src/main/java/party/niwatori/dropgame_scoreboard/DropgameScoreboardApplication.java
+++ src/main/java/party/niwatori/dropgame_scoreboard/DropgameScoreboardApplication.java
@@ -3,6 +3,7 @@ package party.niwatori.dropgame_scoreboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 @EnableJpaAuditing // Enables the use of @CreatedDate and @LastModifiedDate annotations
@@ -10,6 +11,6 @@ public class DropgameScoreboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DropgameScoreboardApplication.class, args);
+        new CountDownLatch(1).await();
 	}
-
 }
